81aabaab8017ca1d083e0a60593f4e69b265cd0c
Sebastian Stegmann <sebastianbille@protonmail.com>
2025-09-26 12:42:49 +0200

    fix

--- install_script.sh
+++ install_script.sh
@@ -8,5 +8,5 @@ psql -U postgres -c "DROP DATABASE IF EXISTS imdb;"
 psql -U postgres -c "create database imdb"
 psql -U postgres -d imdb -f imdb.backup
 psql -U postgres -d imdb -f omdb_data.backup
-psql -U postgres -d imdb -f collected_commands.sql
+psql -U postgres -d imdb -f B2_build_movie_db.sql
 
